178d13452862a6230e1a8c49515982881d2f0f9c
Tao (StarColon) <tao@starcolon.com>
2018-08-30 13:46:21 +0200

    create dotfile

---
+++ sh/dotfile.sh
@@ -0,0 +1,29 @@
+RESET=$(echo -en '\033[0m')
+RED=$(echo -en '\033[00;31m')
+GREEN=$(echo -en '\033[00;32m')
+YELLOW=$(echo -en '\033[00;33m')
+BLUE=$(echo -en '\033[00;34m')
+MAGENTA=$(echo -en '\033[00;35m')
+PURPLE=$(echo -en '\033[00;35m')
+CYAN=$(echo -en '\033[00;36m')
+LRED=$(echo -en '\033[01;31m')
+LGREEN=$(echo -en '\033[01;32m')
+LYELLOW=$(echo -en '\033[01;33m')
+
+findtext(){
+  # arg1 = 'pattern'
+  # clear
+  grep -rnw '.' -e $1 | while read el; do
+    fpath="$(echo $el | cut -d: -f 1)"
+    mt="$(echo $el | cut -d: -f 3 | cut -c 1-60)"
+    printf "${CYAN}$fpath ${RESET}\n"
+    echo -e "\t$mt"
+  done
+}
+
+findtextf(){
+  grep -rnw '.' -e $1 | while read el; do
+    fpath="$(echo $el | cut -d: -f 1)"
+    printf "${CYAN}$fpath ${RESET}\n"
+  done
+}
